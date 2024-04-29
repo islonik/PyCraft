783e5b0d8b3593a6d82649b38d559bb06fe683dd
nikita.lipatov <nikita.lipatov@gmail.com>
2024-04-29 11:09:04 +0100

    add CuisineSerializer

--- lemon/lemon/serializers.py
+++ lemon/lemon/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 
+from .models import Cuisine
 from .models import Meal
 
-class MealSerializer(serializers.ModelSerializer):
+# Allows to display the name of cuisine for Meal
+class CuisineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cuisine
+        fields = ['name']
+
+class MealSerializer(serializers.HyperlinkedModelSerializer):
+    # Could add a hyperlink instead of int id
+    #id = serializers.HyperlinkedRelatedField(many=False, view_name='menu-item-view', read_only=True)
+    cuisine = serializers.StringRelatedField()
     class Meta:
         model = Meal
         fields = ['id', 'name', 'cuisine', 'price', 'desc', 'image', 'image_text']
-        # name = serializers.CharField(max_length = 100)
-        # cuisine = serializers.CharField(max_length = 100)
-        # price = serializers.DecimalField(max_digits = 5, decimal_places = 2)
-        # desc = serializers.CharField(max_length=1000)
-        # image = serializers.CharField(max_length=200)
-        # image_text = serializers.CharField(max_length=200)
--- lemon/lemon/urls.py
+++ lemon/lemon/urls.py
@@ -8,12 +8,12 @@ from . import views
 urlpatterns = [
     # path("api/v1/menu-items",               api.meals,               name="meals"),
     path("api/v1/menu-items",               api.MealsView.as_view()),
-    path("api/v1/menu-items/<int:pk>/",     api.MealView.as_view()),
+    path("api/v1/menu-items/<int:pk>/",     api.MealView.as_view(),  name='menu-item-view'),
     path("",                                views.index,             name='index'),
     path("index/",                          views.index,             name='index'),
     path('about/',                          views.about,             name='about'),
     path('menu/',                           views.menu,              name='menu'),
-    path('menu_item/<int:pk>/',             views.menu_item,         name="menu_item"),
+    path('menu-item/<int:pk>/',             views.menu_item,         name="menu-item"),
     path('booking/',                        BookingView.as_view(),   name="booking"),
     path('bookings/',                       views.bookings,          name='bookings'),
 
